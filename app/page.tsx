ab919f620d0219fa1e18c6934aff5871075c76d0
Lilian Bischung <lilian.bischung@gmail.com>
2025-02-24 08:24:54 +0100

    Fix build

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,30 @@
-
 import React from 'react'
 import WeatherCard from './components/weather/WeatherCard'
 import AddWeatherButton from './components/weather/AddWeatherButton'
+import { PrismaClient } from '@prisma/client'
 
-
+const prisma = new PrismaClient()
 
 export default async function Page() {
-
-  const response = await fetch('http://localhost:3000/api/weather', {
-    method: 'GET'
-  })
-
-  const data = await response.json();
-  
-  
-  
-
-
+  const locations = await prisma.weatherLocation.findMany()
 
   return (
     <>
-
-      
       <div className="h-10"></div>
-
       <div className="weathercards-container flex flex-row justify-center gap-6 flex-wrap">
-
-      {data.map((location: any) => (
-        <WeatherCard
-          key={location.id}
-          id={location.id}
-          location={location.location}
-          temperature={location.temperature}
-          measurementSystem= "°C"
-          description= {location.description}
-          weatherType= {location.weatherType}
+        {locations.map((location) => (
+          <WeatherCard
+            key={location.id}
+            id={location.id}
+            location={location.location}
+            temperature={location.temperature}
+            measurementSystem="°C"
+            description={location.description}
+            weatherType={location.weatherType}
           />
         ))}
-      
-        <AddWeatherButton/>
+        <AddWeatherButton />
       </div>
-        
     </>
   )
 }
