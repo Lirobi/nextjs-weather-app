4cbf7c4620a441fb394598656eb519c56dae2bb7
Lilian Bischung <lilian.bischung@gmail.com>
2025-02-24 08:36:37 +0100

    derniers fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Inter } from 'next/font/google'
 import "./globals.css";
- 
+
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
 })
-const RootLayout = ({children}: {children: React.ReactNode}) => {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="fr" className={`${inter.className} h-full`}>
+      <head>
+        <link rel="icon" href="./logo.svg" type="image/svg+xml" />
+        <title>
+          Lilian Bischung's Weather
+        </title>
+      </head>
       <body className='cursor-default bg-gradient-to-br from-blue-200 via-blue-400 to-blue-600 h-full w-full  '>
         {children}
       </body>
